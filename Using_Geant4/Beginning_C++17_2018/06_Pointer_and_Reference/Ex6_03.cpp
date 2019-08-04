4bddefd9c67c840b17a8d61e3bac2ec3c5a7aaf7
Lang-Shao <lang@pmo.ac.cn>
2019-08-04 12:10:30 +0800

    pointer to char string

---
+++ Using_Geant4/Beginning_C++17_2018/06_Pointer_and_Reference/Ex6_03.cpp
@@ -0,0 +1,20 @@
+// Initializing pointers with strings
+#include  <iostream>
+
+int main()
+{
+	int count {};
+	int* pcount {&count};
+
+	*pcount = 12;
+	std::cout << "The address of count (pcount) is " << pcount << std::endl;
+	std::cout << "The count is " << *pcount << std::endl;
+
+	const char* pstar1{ "Fatty Arbuckle" };
+
+	std::cout << "The first letter of your lucky star is " << *pstar1 << std::endl;
+	std::cout << "Your lucky star is " << pstar1 << std::endl;
+
+
+
+}
