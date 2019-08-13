2da8343de4cd9a50b4d3143256dcca7590eee072
Lang-Shao <lang@pmo.ac.cn>
2019-08-13 13:23:19 +0800

    first GUI

--- Using_Java/Java_Programming_2019/01_Creating_Java_Programs/Helloworld.java
+++ Using_Java/Java_Programming_2019/01_Creating_Java_Programs/Helloworld.java
@@ -1,7 +1,14 @@
-public class Helloworld
+public class Helloworld // must same with file name, i.e., Helloworld.java
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world!");
+		System.out.println("First time:");
+		System.out.print("Hello ");
+		System.out.print("worlld!");
+
+		System.out.println("\nSecond time:");
+		System.out.println("Hello world!"); //semicolon can't be omitted
+		
+
 	}
 }
