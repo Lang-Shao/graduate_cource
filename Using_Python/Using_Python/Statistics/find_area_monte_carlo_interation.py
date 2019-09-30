b2f8288bf98c741bccba9f1dbc5e83648dcafa3b
Lang-Shao <lang@pmo.ac.cn>
2019-09-30 10:46:55 +0800

    update

--- Using_Python/Using_Python/Statistics/find_area_monte_carlo_interation.py
+++ Using_Python/Using_Python/Statistics/find_area_monte_carlo_interation.py
@@ -2,20 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def f1(x):
-	"""up small circle"""
-	return (5**2.0-(x-5.0)**2.0)**0.5+5.0
+    """up small circle"""
+    return (5**2.0-(x-5.0)**2.0)**0.5+5.0
 
 def f2(x):
-	"""down small circle"""
-	return -(5**2.0-(x-5.0)**2.0)**0.5+5.0
+    """down small circle"""
+    return -(5**2.0-(x-5.0)**2.0)**0.5+5.0
 
 def f3(x):
-	"""up big circle"""
-	return (10**2.0-(x-10.0)**2.0)**0.5
+    """up big circle"""
+    return (10**2.0-(x-10.0)**2.0)**0.5
 
 def f4(x):
-	"""down big circle"""
-	return -(10**2.0-x**2.0)**0.5+10
+    """down big circle"""
+    return -(10**2.0-x**2.0)**0.5+10
 
 x=np.linspace(0,10,201)
 
@@ -26,20 +26,20 @@ d_x = np.random.uniform(0,10,npoints)
 d_y = np.random.uniform(0,10,npoints)
 
 for i in range(npoints):
-	if ((f1(d_x[i]) > d_y[i] 
-		and f2(d_x[i]) < d_y[i] 
-		and f3(d_x[i]) < d_y[i]) or 
-		(f2(d_x[i]) < d_y[i]
-		and f4(d_x[i]) > d_y[i]
-		and f1(d_x[i]) > d_y[i])):
-		goodpoints_x.append(d_x[i])
-		goodpoints_y.append(d_y[i])
+    if ((f1(d_x[i]) > d_y[i] 
+        and f2(d_x[i]) < d_y[i] 
+        and f3(d_x[i]) < d_y[i]) or 
+        (f2(d_x[i]) < d_y[i]
+        and f4(d_x[i]) > d_y[i]
+        and f1(d_x[i]) > d_y[i])):
+        goodpoints_x.append(d_x[i])
+        goodpoints_y.append(d_y[i])
 '''
 for i in range(npoints):
-	if (f1(d_x[i]) > d_y[i] 
-		and f2(d_x[i]) < d_y[i]):
-		goodpoints_x.append(d_x[i])
-		goodpoints_y.append(d_y[i])
+    if (f1(d_x[i]) > d_y[i] 
+        and f2(d_x[i]) < d_y[i]):
+        goodpoints_x.append(d_x[i])
+        goodpoints_y.append(d_y[i])
 '''
 
 area=100.0*(len(goodpoints_x)/len(d_x))
