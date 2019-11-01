b6bd655044e161f3e5fdc5bd42f3796833c7a011
Lang-Shao <lang@pmo.ac.cn>
2019-11-01 09:14:23 +0800

    select database using hostname

--- Using_Python/My_Codes/Fermi/GBM/personal_settings.py
+++ Using_Python/My_Codes/Fermi/GBM/personal_settings.py
@@ -1,16 +1,22 @@
 # modify this file for personal settings
 # the changes of which should be ignored during each commit
 
+import socket
+
 def get_usersjar():
 	usersjar = "/home/lang/Software/HEASARC-Xamin/users.jar"
 	return usersjar
 
 def get_burstdatabasedir():
-	databasedir = '/diskb/Database/Fermi/gbm_burst/data/'
-	#databasedir = '/home/lang/work/GBM/burstdownload/data/'
+	if socket.gethostname() == 'SHebtu':
+		databasedir = '/diskb/Database/Fermi/gbm_burst/data/'
+	else:
+		databasedir = '/home/lang/work/GBM/burstdownload/data/'
 	return databasedir
 
 def get_dailydatabasedir():
-	#dailydatabasedir= '/diska/Fermi_GBM_daily/data/'
-	dailydatabasedir = '/home/lang/work/GBM/daily/data/'
+	if socket.gethostname() == 'SHebtu':
+		dailydatabasedir= '/diska/Fermi_GBM_daily/data/'
+	else:
+		dailydatabasedir = '/home/lang/work/GBM/daily/data/'
 	return dailydatabasedir
