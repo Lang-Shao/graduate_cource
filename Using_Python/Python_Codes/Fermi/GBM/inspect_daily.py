6c522bf7e4ef79aab608163d307090f797e6e3d6
Lang-Shao <lang@pmo.ac.cn>
2019-11-08 02:47:46 +0800

    update

--- Using_Python/Python_Codes/Fermi/GBM/inspect_daily.py
+++ Using_Python/Python_Codes/Fermi/GBM/inspect_daily.py
@@ -6,7 +6,6 @@ matplotlib.use('Agg')
 def inspect_timewindow(winname, StartUTC, EndUTC, resultdir='./'):
 	slice1= TIMEWINDOW(winname, StartUTC, EndUTC, resultdir=resultdir)
 	slice1.plot_base()
-	#slice1.binned_netlc()
 	slice1.plot_netlc()
 	slice1.check_netlc_gaussian_distribution()
 	slice1.plot_netlc_significance()
