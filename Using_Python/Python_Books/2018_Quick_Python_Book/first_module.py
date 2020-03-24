017ffadaeb1bf50947998b9266c93199627c0900
Lang-Shao <lang@pmo.ac.cn>
2020-03-24 13:56:02 +0800

    add new codes for chapters

---
+++ Using_Python/Python_Books/2018_Quick_Python_Book/first_module.py
@@ -0,0 +1,17 @@
+import sys
+
+def myfun():
+	sfas
+
+def main():
+	pi = 3.14159
+	def area(r):
+    		"""area(r): return the area of a circle with radius r."""
+    		global pi
+    		return(pi * r * r)
+	print('pi=',pi)
+	print(sys.path)
+
+
+
+main()
